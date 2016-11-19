15e482ac94b98bd8cb33dfda010e4aa3f1827c9f
verusmick <miguel.angel.angulo72@gmail.com>
2016-11-19 17:10:37 -0400

    I added highcharts library.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,6 +9,22 @@
   function MainController($timeout, webDevTec, toastr) {
     var vm = this;
 
+    Highcharts.chart('container', {
+      title: {
+        text: 'Temperature Data'
+      },
+
+      xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        ]
+      },
+
+      series: [{
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+      }]
+    });
+
     vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1479527967795;
