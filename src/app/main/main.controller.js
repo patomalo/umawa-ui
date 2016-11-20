814dd23e54e4840d31857c5b1a875286c0d4a9d3
kian182 <folieadeux182>
2016-11-20 13:19:35 -0400

    GG la vida

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -10,6 +10,13 @@
     var vm = this;
     $scope.sidebarFlag = false;
 
+    $scope.flags = {
+      uno:false,
+      dos:false,
+      tres:false,
+      cuatro:false
+    };
+
     vm.initialize = function () {
       EventsServices.start(function(msg){
         // console.log('msg', msg)
@@ -20,6 +27,7 @@
       $scope.sidebarFlag = !$scope.sidebarFlag;
       if($scope.sidebarFlag){
         $scope.openNav();
+        $scope.clickSensor({code:'luz'})
       }
       else{
         $scope.closeNav();
@@ -44,22 +52,91 @@
 
     $scope.clickSensor = function(sensor){
       console.log('sensor', sensor);
+      if(sensor.code === 'luz'){
+        $scope.flags.uno = true;
+        $scope.flags.dos = false;
+        $scope.flags.tres = false;
+        $scope.flags.cuatro = false;
+      }
+      if(sensor.code === 'hsuelo'){
+        $scope.flags.uno = false;
+          $scope.flags.dos = true;
+          $scope.flags.tres = false;
+          $scope.flags.cuatro = false;
+      }
+      if(sensor.code === 'hrelativa'){
+        $scope.flags.uno = false;
+          $scope.flags.dos = false;
+          $scope.flags.tres = true;
+          $scope.flags.cuatro = false;
+      }
+      if(sensor.code === 'flujo'){
+        $scope.flags.uno = false;
+          $scope.flags.dos = false;
+          $scope.flags.tres = false;
+          $scope.flags.cuatro = true;
+      }
+
+      // $scope.flags.
+        // if(){}
     };
 
 
-    // Highcharts.chart('container', {
-    //   title: {
-    //     text: 'Temperature Data'
-    //   },
-    //   xAxis: {
-    //     categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-    //       'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-    //     ]
-    //   },
-    //   series: [{
-    //     data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-    //   }]
-    // });
+    Highcharts.chart('container1', {
+      title: {
+        text: 'Luz'
+      },
+      xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        ]
+      },
+      series: [{
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+      }]
+    });
+
+    Highcharts.chart('container2', {
+      title: {
+        text: 'Humedad del Suelo'
+      },
+      xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        ]
+      },
+      series: [{
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+      }]
+    });
+
+    Highcharts.chart('container3', {
+      title: {
+        text: 'Humedad Relativa'
+      },
+      xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        ]
+      },
+      series: [{
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+      }]
+    });
+
+    Highcharts.chart('container4', {
+      title: {
+        text: 'Flujo'
+      },
+      xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        ]
+      },
+      series: [{
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+      }]
+    });
 
     vm.initialize();
   }
