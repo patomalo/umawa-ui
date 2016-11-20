e25110781ffb52ebabd97a4827a2a75b481f25c9
verusmick <miguel.angel.angulo72@gmail.com>
2016-11-20 07:53:40 -0400

    changed param from main.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,12 +8,13 @@
   /** @ngInject */
   function MainController($scope, $timeout, webDevTec, toastr, EventsServices) {
     var vm = this;
+    $scope.sidebarFlag = false;
 
     vm.initialize = function () {
-
+      EventsServices.start(function(msg){
+        // console.log('msg', msg)
+      });
     };
-
-    $scope.sidebarFlag = false;
 
     $scope.switchSidebar = function(){
       $scope.sidebarFlag = !$scope.sidebarFlag;
@@ -26,34 +27,34 @@
       EventsServices.start(pipeBroke);
     };
     var pipeBroke = function(event){
-      // console.log(JSON.parse(event.data));
+      console.log(JSON.parse(event.data));
     };
 
     $scope.openNav = function () {
       document.getElementById("mySidenav").style.width = "42%";
       document.getElementById("main").style.marginLeft = "58%";
+      $scope.sidebarFlag = true;
     };
 
     $scope.closeNav = function(){
       document.getElementById("mySidenav").style.width = "0";
       document.getElementById("main").style.marginLeft= "0";
+      $scope.sidebarFlag = false;
     };
 
-    // Highcharts.chart('container', {
-    //   title: {
-    //     text: 'Temperature Data'
-    //   },
-    //
-    //   xAxis: {
-    //     categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-    //       'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-    //     ]
-    //   },
-    //
-    //   series: [{
-    //     data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-    //   }]
-    // });
+    Highcharts.chart('container', {
+      title: {
+        text: 'Temperature Data'
+      },
+      xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        ]
+      },
+      series: [{
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+      }]
+    });
 
     vm.initialize();
   }
