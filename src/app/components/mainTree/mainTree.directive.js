da5a97a8abb4cd067357c61f28d668d03618178d
verusmick <miguel.angel.angulo72@gmail.com>
2016-11-20 04:43:55 -0400

    Removed event in panel.

--- src/app/components/mainTree/mainTree.directive.js
+++ src/app/components/mainTree/mainTree.directive.js
@@ -170,10 +170,10 @@
         // Toggle children on click.
         function click(d) {
           if (d.children) {
-            // d._children = d.children;
+            d._children = d.children;
             d.children = null;
           } else {
-            // d.children = d._children;
+            d.children = d._children;
             d._children = null;
           }
           update(d);
